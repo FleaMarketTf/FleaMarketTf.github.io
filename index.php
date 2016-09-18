4000ca0f939cdf385f3e87b15262eea29e430fbb
noobularkid <noobularkid@gmail.com>
2016-09-18 01:45:49 -0400

    test

--- index.php
+++ index.php
@@ -14,5 +14,29 @@
 		if(!isset($_SESSION['T2SteamAuth'])){
 			$login = "<div id=\"login\">Welcome Guest!<img src=\"steamcommunity.com/public/images/signinthroughsteam/sits_small.png\"/></a> to *website action*.com</div>";
 		}
+		
+	}elseif($OpenID->node == "cancel"){
+		echo "User Authentication has failed.";
+
+	}else(if(!isset($_SESSION['T2SteamAuth']))){
+		$_SESSION['T2SteamAuth'] = $OpenID->validate() ? $OpenID->indentity : null;
+		$_SESSION['T2SteamAuth'] = str_replace("http://steamcomunity.com/openid/id/", "", $_SESSION['T2SteamAuth']);
+		if($_SESSION['T2SteamAuth'] !== null){
+			$Steam64 = str_replace("http://steamcomunity.com/openid/id/","",$_SESSION['T2SteamAuth'])
+			$Profile = file_get_contents("httpL//api.steampowered.com/ISteamUser/GetPlayerSumaries/v0002/?key=($api)$steamids=($Steam64)");
+			$buffer = fopen("cache/($Steam64).json","w+");
+			fwrite($buffer, $Profile);
+			fclose($buffer);
+		}
+		header("Location: index.php");
 	}
+	if(isset($_SESSION['T2SteamAuth'])){
+		$login = "<div id=\"login\"><a href=\"?logout\">logout</a></div>";
+	}
+	if(isset($_GET['logout'])){
+		unset($_SESSION['T2SteamAuth']);
+		unset($_SESSION['T2SteamID64']);
+		header("Location: index.php");
+	}
+echo $login;
 ?>
