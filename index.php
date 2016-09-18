f69118b127615f9fa7bc2b734f7304fac8ec2db4
noobularkid <noobularkid@gmail.com>
2016-09-18 12:17:15 -0400

    test

--- index.php
+++ index.php
@@ -1,21 +1,6 @@
-			<?php
-
-				require 'steamauth/steamauth.php';
-
-				?>
-			<?php
-			if(!isset($_SESSION['steamid'])) {
-
-			    loginbutton(); //login button
-
-			}  else {
-
-			    include ('steamauth/userInfo.php'); //To access the $steamprofile array
-			    //Protected content
-
-			    logoutbutton(); //Logout Button
-			}     
-			?>
+<?php
+	require 'steamauth/steamauth.php';
+?>
 <!DOCTYPE html>
 <html>
 <head>
